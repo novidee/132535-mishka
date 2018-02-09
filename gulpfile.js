a24593edb78219542743ebcde0254779d756f61b
Дмитрий Новиков <mr.ronuar@gmail.com>
2018-02-09 19:47:47 +0300

    Настраивает минификацию изображений

--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,90 @@
-"use strict";
+`use strict`;
 
-var gulp = require("gulp");
-var less = require("gulp-less");
-var plumber = require("gulp-plumber");
-var postcss = require("gulp-postcss");
-var autoprefixer = require("autoprefixer");
-var minify = require("gulp-csso");
-var svgstore = require("gulp-svgstore");
-var rename = require("gulp-rename");
-var del = require("del");
-var server = require("browser-sync").create();
+var gulp = require(`gulp`);
+var less = require(`gulp-less`);
+var plumber = require(`gulp-plumber`);
+var postcss = require(`gulp-postcss`);
+var autoprefixer = require(`autoprefixer`);
+var minify = require(`gulp-csso`);
+var svgstore = require(`gulp-svgstore`);
+var imagemin = require(`gulp-imagemin`);
+var rename = require(`gulp-rename`);
+var del = require(`del`);
+var server = require(`browser-sync`).create();
 
-gulp.task("style", function() {
-  gulp.src("source/less/style.less")
+var sourcePath = `source`;
+var buildPath = `build`;
+
+gulp.task(`style`, function() {
+  gulp.src(`${sourcePath}/less/style.less`)
     .pipe(plumber())
     .pipe(less())
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("build/css"))
+    .pipe(gulp.dest(`${buildPath}/css`))
     .pipe(minify())
-    .pipe(rename("style.min.css"))
-    .pipe(gulp.dest("build/css"))
+    .pipe(rename(`style.min.css`))
+    .pipe(gulp.dest(`${buildPath}/css`))
     .pipe(server.stream());
 });
 
-gulp.task("sprite", function () {
-  return gulp.src("source/img/icon-*.svg")
+gulp.task(`images`, function () {
+  return gulp.src(`${buildPath}/img/**/*.{png,jpg,svg}`)
+    .pipe(imagemin([
+      imagemin.optipng({ optimizationLevel: 3 }),
+      imagemin.jpegtran({ progressive: true }),
+      imagemin.svgo({
+        plugins: [{
+          cleanupNumericValues: {
+            floatPrecision: 0
+          }
+        }]
+      })
+    ]))
+    .pipe(gulp.dest(`${buildPath}/img`));
+});
+
+gulp.task(`sprite`, function () {
+  return gulp.src(`${buildPath}/img/icon-*.svg`)
+    .pipe(imagemin([
+      imagemin.svgo({
+        plugins: [{
+          removeAttrs: { attrs: '(stroke|fill)' }
+        }]
+      })
+    ]))
     .pipe(svgstore({
       inlineSvg: true
     }))
-    .pipe(rename("sprite.svg"))
-    .pipe(gulp.dest("build/img"));
+    .pipe(rename(`sprite.svg`))
+    .pipe(gulp.dest(`${buildPath}/img`));
 });
 
-gulp.task("serve", ["style"], function() {
+gulp.task(`serve`, [`style`], function() {
   server.init({
-    server: "source/",
+    server: `${sourcePath}/`,
     notify: false,
     open: true,
     cors: true,
     ui: false
   });
 
-  gulp.watch("source/less/**/*.less", ["style"]);
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch(`${sourcePath}/less/**/*.less`, [`style`]);
+  gulp.watch(`${sourcePath}/*.html`).on(`change`, server.reload);
 });
 
-gulp.task("clean", function() {
-  return del("build");
+gulp.task(`clean`, function() {
+  return del(buildPath);
 });
 
-gulp.task("copy", function () {
+gulp.task(`copy`, function () {
   return gulp.src([
-    "source/fonts/**/*.{woff,woff2}",
-    "source/img/**",
-    "source/js/**"
+    `${sourcePath}/fonts/**/*.{woff,woff2}`,
+    `${sourcePath}/img/**`,
+    `${sourcePath}/js/**`
   ], {
-      base: "source"
+      base: sourcePath
   })
-    .pipe(gulp.dest("build"));
+    .pipe(gulp.dest(buildPath));
 });
